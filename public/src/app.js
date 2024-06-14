99e3c105c04f36c5cfb25a2517f2950f6a54d81e
Nahed Al Awlaki <Nahed.atik@hotmail.com>
2024-06-14 02:49:36 +0200

    updated versions

---
+++ public/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DataDisplay from './DataDisplay';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Application</h1>
+      <DataDisplay /> {/* This is where DataDisplay component is inserted */}
+    </div>
+  );
+}
+
+export default App;
+
